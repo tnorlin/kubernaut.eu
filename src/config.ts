c6315d75ce4701f33b3fffa44bd33d316a8c413a
Tony Norlin <tony.norlin@localdomain.se>
2023-08-26 18:57:17 +0200

    Update the configuration
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

---
+++ src/config.ts
@@ -0,0 +1,125 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://kubernaut.eu/",
+  author: "Tony Norlin",
+  desc: "A technical blog about the home lab adventures of the Kubernaut.",
+  title: "kubernaut.eu",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 5,
+};
+
+export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/tnorlin",
+    linkTitle: `tnorlin on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/tonynorlin/",
+    linkTitle: `tnorlin on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:tnorlin@kubernaut.eu",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/tnorlin",
+    linkTitle: `tnorlin on Twitter`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://k8s.social/@tnorlin",
+    linkTitle: `tnorlin on Mastodon`,
+    active: true,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,11 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About kubernaut"
+---
+
+The Kubernaut is a blog about technologies that captured the interest of its author Tony Norlin. The author also has an interest in Italian food culture and intends to write about pasta, pizza and coffe at the [personal blog](https://tnorlin.se).
+
+
+<div>
+  <img src="/assets/about.png" class="sm:w-1/1 " alt="captain Kube exploring the greek waters">
+</div>
