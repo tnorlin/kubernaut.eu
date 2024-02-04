59f3bd1c346c9c6144edf0385050760d15bfdb87
Tony Norlin <tony.norlin@localdomain.se>
2024-02-04 14:37:45 +0100

    Add files
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

---
+++ src/components/Comments.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Giscus from "@giscus/react";
+
+const id = "inject-comments";
+
+const Comments = () => {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted ? (
+    <Giscus
+      id={id}
+      repo="tnorlin/kubernaut.eu"
+      repoId="R_kgDOIXRBqw"
+      category="Announcements"
+      categoryId="DIC_kwDOIXRBq84CY4YN"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      lang="en"
+      loading="lazy"
+    />
+  ) : null;
+};
+
+export default Comments;
