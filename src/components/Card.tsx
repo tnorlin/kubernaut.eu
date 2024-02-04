180add81f7ac1ae500b12e728b962725c640412c
Tony Norlin <tony.norlin@localdomain.se>
2024-02-04 15:32:55 +0100

    Test
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,8 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, minutesRead, description } =
-    frontmatter;
+  const { title, pubDatetime, modDatetime, description } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -29,11 +28,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
           <h3 {...headerProps}>{title}</h3>
         )}
       </a>
-      <Datetime
-        pubDatetime={pubDatetime}
-        modDatetime={modDatetime}
-        readtime={minutesRead}
-      />
+      <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
       <p>{description}</p>
     </li>
   );
