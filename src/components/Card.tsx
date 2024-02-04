4213ca727aabf366cbe9875f020decb323aae381
Tony Norlin <tony.norlin@localdomain.se>
2024-02-04 16:18:40 +0100

    Test
    Signed-off-by: Tony Norlin <tony.norlin@localdomain.se>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -28,11 +28,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
           <h3 {...headerProps}>{title}</h3>
         )}
       </a>
-      <Datetime
-        pubDatetime={pubDatetime}
-        modDatetime={modDatetime}
-        readtime={minutesRead}
-      />
+      <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
       <p>{description}</p>
     </li>
   );
